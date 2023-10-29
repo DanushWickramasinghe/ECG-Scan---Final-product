9fb4981ef0bbe512b9606a1dba9b4cde0534e0cc
ow1212 <wijesooriyawador.20@uom.lk>
2023-10-29 16:47:18 +0530

    About page

--- Client/src/Views/About.js
+++ Client/src/Views/About.js
@@ -3,44 +3,45 @@ import React from "react";
 const About = () => {
   return (
     <div className="about-page">
-      <h1>About ReadECG - Your ECG Analysis Solution</h1>
-      <p>
-        Welcome to ReadECG, a cutting-edge ECG analysis solution that embodies our passion for healthcare innovation and the relentless pursuit of improving patient care.
-      </p>
+      <h1>ReadECG - Your ECG Analysis Solution</h1>
+      <div>
+        <h3>Welcome to ReadECG, a cutting-edge ECG analysis solution that embodies our passion for healthcare innovation and the relentless pursuit of improving patient care.</h3>
+      </div>
 
-      <h2>Our Vision</h2>
-      <p>
-        Our vision is to lead the transformation of healthcare through technology. We envision a world where the diagnosis and treatment of cardiovascular diseases are more accessible, accurate, and efficient.
-      </p>
-      <p>
-        ReadECG is our commitment to achieving this vision by streamlining ECG interpretation, providing automated identification and categorization of cardiac irregularities, reducing the time and skill required, and enabling early detection for improved patient outcomes.
-      </p>
+      <div>
+        <h2>Our Vision</h2>
+        <p>Our vision is to lead the transformation of healthcare through technology. We envision a world where the diagnosis and treatment of cardiovascular diseases are more accessible, accurate, and efficient.</p>
+      </div>
 
-      <h2>Our Mission</h2>
-      <p>
-        At the heart of our mission is a commitment to advancing the diagnosis and treatment of cardiovascular diseases. We believe that by leveraging the power of artificial intelligence and deep learning, ReadECG can revolutionize the field of electrocardiography. It's not just a product; it's a vision realized.
-      </p>
+      <div>
+        <h2>Our Mission</h2>
+        <p>At the heart of our mission is a commitment to advancing the diagnosis and treatment of cardiovascular diseases. We believe that by leveraging the power of artificial intelligence and deep learning, ReadECG can revolutionize the field of electrocardiography. It's not just a product; it's a vision realized.</p>
+      </div>
 
-      <h2>Our Solution</h2>
-      <p>
-        ReadECG is a state-of-the-art ECG Analyzer based on Convolutional Neural Networks (CNNs) that are designed to excel in the identification and interpretation of electrocardiogram (ECG) signals. Our solution provides accurate and rapid analysis of ECG data, enabling healthcare professionals to make informed decisions faster than ever before.
-      </p>
+      <div>
+        <h2>Our Solution</h2>
+        <p>ReadECG is a state-of-the-art ECG Analyzer based on Convolutional Neural Networks (CNNs) that are designed to excel in the identification and interpretation of electrocardiogram (ECG) signals. Our solution provides accurate and rapid analysis of ECG data, enabling healthcare professionals to make informed decisions faster than ever before.</p>
+      </div>
 
-      <h2>What Sets ReadECG Apart?</h2>
-      <p>
-        <strong>Accuracy:</strong> ReadECG is built on advanced machine learning algorithms that have been rigorously trained on vast datasets, ensuring the highest level of accuracy in ECG interpretation.
-      </p>
-      <p>
-        <strong>Speed:</strong> We understand that in healthcare, time is of the essence. ReadECG provides rapid ECG analysis, empowering healthcare providers to make swift decisions and potentially save lives.
-      </p>
-      <p>
-        <strong>Accessibility:</strong> ReadECG is committed to making ECG analysis accessible to a broad audience. Our user-friendly interface ensures that healthcare professionals of all levels can benefit from our technology.
-      </p>
-      <p>
-        <strong>Continuous Improvement:</strong> We are dedicated to ongoing research and development, always striving to improve ReadECG's performance and expand its capabilities.
-      </p>
+      <div>
+        <h2>What Sets ReadECG Apart?</h2>
+        <p>Accuracy: ReadECG is built on advanced machine learning algorithms that have been rigorously trained on vast datasets, ensuring the highest level of accuracy in ECG interpretation.</p>
+        <p>Speed: We understand that in healthcare, time is of the essence. ReadECG provides rapid ECG analysis, empowering healthcare providers to make swift decisions and potentially save lives.</p>
+        <p>Accessibility: ReadECG is committed to making ECG analysis accessible to a broad audience. Our user-friendly interface ensures that healthcare professionals of all levels can benefit from our technology.</p>
+        <p>Continuous Improvement: We are dedicated to ongoing research and development, always striving to improve ReadECG's performance and expand its capabilities.</p>
+      </div>
+
+      <div>
+        <h2>Team Behind ReadECG</h2>
+        <p>Our team comprises dedicated data scientists, pursuing engineers who are passionate about the intersection of technology and medicine. Together, we have harnessed our collective expertise to create a product to addresses the challenge of the growing number of ECGs in the healthcare industry.</p>
+      </div>
+
+      <div>
+        <h2>Contact Us</h2>
+        <p>If you have any questions or would like to learn more about ReadECG, please don't hesitate to reach out. Together, with ReadECG, we can make a difference in the world of medical diagnostics.</p>
+      </div>
     </div>
   );
-};
+}
 
 export default About;
